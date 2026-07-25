480ccfa9639a6c005ddfeecd3252b021fa4796f8
erick.giraldo <erick.giraldo@decisionnext.com>
2026-07-24 19:37:34 -0500

    refactor: standardize output filename variables and add translation processing for job descriptions

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ llm_models = [
 
 if __name__ == "__main__":
     # Parsing CVs
-    output_filename = "data/cv_extracted_info.json"
+    cvs_output_filename = "data/cv_extracted_info.json"
     cvs_parser = DocsParser(
         response_model= Applicant,
         llm_models=llm_models,
-        path=output_filename
+        path=cvs_output_filename
         )
     cvs_data = cvs_parser.gather_docs_info(
         dir = "data/CVs",
@@ -28,15 +28,15 @@ if __name__ == "__main__":
     # Loading cvs and translating to English
     cvs_parser.process_and_translate_docs(
         output_filename = "data/cv_extracted_info_eng.json",
-        max_docs = 2,
-        file_path = output_filename
+        file_path = cvs_output_filename
     )
 
+    positions_output_filename = "data/job_descriptions_extracted_info.json"
     # Parsing "Vacantes"
     positions_parser = DocsParser(
         response_model= Position,
         llm_models=llm_models,
-        path="data/job_descriptions_extracted_info.json",
+        path=positions_output_filename,
         )
     positions_data = positions_parser.gather_docs_info(
         dir = "data/Vacantes",
@@ -47,3 +47,8 @@ if __name__ == "__main__":
     positions_parser.save_extracted_data(
         data = positions_data
         )
+    # Loading job descriptions and translating to English
+    positions_parser.process_and_translate_docs(
+        output_filename = "data/job_descriptions_extracted_info_eng.json",
+        file_path = positions_output_filename
+    )
